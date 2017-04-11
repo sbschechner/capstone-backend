8b72e50701e38816c42adc30a9a851cb6b499eea
Think <think@Thinks-MacBook-Air.local>
2017-04-11 00:12:53 -0400

    building Get Request and Server

--- server.js
+++ server.js
@@ -1,14 +1,75 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+
+//import variable section
+var {PORT, DATABASE_URL} = require('./config');
+var {Expenses} = require('./models');
+
+
+
 var app = express();
-
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
-console.log("up and running");
+app.use(morgan('common'));
+app.use(bodyParser.json);
+
+
+//get request
+app.get("/expenseTracker", function(request, response){
+	//MANIUPLATION OF THE MONGO DB TO RETURN GET
+})
 
 
 
-/*if (require.main === module){
+// in case someone uses an endpoing that doesn't exist
+app.use("*", function(request,response){
+	response.status(404).json({message: "Not Found"});
+	});
+
+
+//connect to DB
+var server;
+
+function runServer(database_URL = DATABASE_URL, port = PORT ){
+	return new Promise((resolve, reject)=>{
+		mongoose.connect(database_URL, error =>{
+			if (error){
+				console.log("Server Connection Not Working")
+				return reject(error);
+			}
+			server = app.listen(port, () => {
+				console.log("everything connected");
+				resolve();
+			})
+			.on("error", error => {
+				mongoose.disconnect();
+				reject(error);
+			});
+		});
+	});
+}
+
+function closeServer(){
+	return mongoose.disconnect().then(()=> {
+		return new Promise((resolve, reject) =>{
+			console.log("closing the server");
+			server.close(error => {
+				if (error){
+					return reject(error);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+
+//so I can use these runServer and close server functions in testing file
+if (require.main === module){
 	runServer().catch(error => console.log(error));
-}; */
+};
 
-module.exports = {app};
+module.exports = {app, runServer, closeServer};
--- test/test-expense-tracker.js
+++ test/test-expense-tracker.js
@@ -5,9 +5,14 @@ var faker = require('faker');
 
 var should = chai.should() //allows for the should.be syntax
 
-var {app} = require("../server");
+var {app, runServer, closeServer} = require("../server");
+var {ExpenseGrouper} = require("../models");
+var {TEST_DATABASE_URL} = require("../config");
 
 chai.use(chaiHttp);
+
+
+//NEED TO CREATE SEEDING TO TEST GET REQUEST
 
 describe("making sure page is up and running", function(){
 	it("returns HTML", function(){
@@ -15,6 +20,7 @@ describe("making sure page is up and running", function(){
 			.get("/")
 			.then(function(response){
 				response.should.have.status(200);
+				response.should.be.HTML;
 			});
 	});
 
