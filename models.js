e5cf4456bdbd8c9ad55029957fc2b7337b1dfd83
Think <think@Thinks-MacBook-Air.local>
2017-04-11 00:23:04 -0400

    laid out schema

--- models.js
+++ models.js
@@ -1,1 +1,14 @@
-models.js
+var mongoose = require("mongoose");
+
+var expenseSchema = mongoose.Schema({
+	name: 
+	amount:
+	assignee:
+
+});
+
+//need to create virtuals and ApiReturn function 
+
+var Expenses = mongoose.model("Expenses", expenseSchema);
+
+module.exports{Expenses}
